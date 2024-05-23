b8c4f0998c8c930368a8336a5361f7d550ac406b
elphan <starsoccerethan@gmail.com>
2024-05-23 14:18:56 -0400

    updated server to take in env variables...

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 
 // handling MongoDB connection
-mongoose.connect('mongodb://localhost:27017/chat_messages');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const database = mongoose.connection;
 database.on('error', (error) => console.error('Database connection error:', error));
