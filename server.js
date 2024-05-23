f37071e08c412e4f13a771be7055ce1d9c491c75
elphan <starsoccerethan@gmail.com>
2024-05-23 14:42:52 -0400

    remove CORS from the server...deployment successful! API path have not been adjusted.

--- server.js
+++ server.js
@@ -28,17 +28,6 @@ const Data = mongoose.model('chat', dataSchema);
 
 app.use(express.json());
 
-// handling CORS 
-app.use((req, res, next) => { 
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-}); 
 
 // Serve static files from the Angular app
 app.use(express.static(path.join(__dirname, 'dist/simply-chat-frontend')));
