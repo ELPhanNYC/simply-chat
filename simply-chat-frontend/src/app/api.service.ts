7048cfe67136625252da42cf58008049ed5d7f4b
elphan <starsoccerethan@gmail.com>
2024-05-23 13:55:22 -0400

    updated project build for deployment

--- simply-chat-frontend/src/app/api.service.ts
+++ simply-chat-frontend/src/app/api.service.ts
@@ -9,9 +9,9 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getMessage() {
-    return this.http.get('http://localhost:3000/api/getmessage');
+    return this.http.get('http://localhost:8080/api/getmessage');
   }
   postMessage(data: { alias: string; message: string }) {
-    return this.http.post('http://localhost:3000/api/post', data);
+    return this.http.post('http://localhost:8080/api/post', data);
   }
 }
